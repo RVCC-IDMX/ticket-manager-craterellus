a801614a3c4a766019e7a16ea579691ab4a5d612
craterellus <98069492+craterellus@users.noreply.github.com>
2022-10-23 22:16:41 -0400

    Complete step 1 of nodejs tutorial

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Meowmeow');
+const chalk = require('chalk');
+
+const { log } = console;
+
+// Basic colors
+log(chalk.red('Wahahaha red'));
+log(chalk.magenta('Meowmeowmeow meow-genta'));
+log(chalk.yellow('Ring ring ring ring ring bananaphone'));
+
+// Background colors
+log(chalk.bgRed('Hello Hello testing red fred'));
+log(chalk.bgGreen('It is not easy being green.'));
+
+// Styling & decoration
+log(chalk.bold('This is bold'));
+log(chalk.underline('This is underlined'));
+
+// Chaining
+log(chalk.yellow.bgRed.underline('Yahoo!!!'));
